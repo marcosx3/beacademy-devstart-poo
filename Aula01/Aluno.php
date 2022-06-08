3d0af93905817cf460eb6610940536a981a14265
marcosx3 <marcos-oliveirasilva@hotmail.com>
2022-06-08 08:28:19 -0300

    0.0.1

---
+++ Aula01/Aluno.php
@@ -0,0 +1,20 @@
+<?php 
+declare(strict_types=1);
+class Aluno {
+    private string $nome;
+    private string $cpf;
+
+    function __construct(string $nome,string $cpf)
+    {
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+    }
+
+
+
+
+
+
+}
+
+?>
